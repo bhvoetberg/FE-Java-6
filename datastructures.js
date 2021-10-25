12551ba190d62302b3919a94e58c8476d53aa2bd
bhvoetberg <b.h.voetberg@gmail.com>
2021-10-25 12:41:43 +0200

    First commit

---
+++ datastructures.js
@@ -0,0 +1,48 @@
+// let students = ['Henk', 'Jan', 'Piet', 'Freek', 'Stabila '];
+
+let students = [
+    {
+        name: 'Henk',
+        course: 'FSD',
+        age: 28
+    },
+    {
+        name: 'Piet',
+        course: 'FE',
+        age: 38
+    },
+    {
+        name: 'Jan',
+        course: 'BE',
+        age: 98
+    },
+    {
+        name: 'Frank',
+        course: 'FE',
+        age: 8
+    },
+    {
+        name: 'Theo',
+        age: 48
+    }
+]
+
+console.log(students[1].course);
+
+let littleStudents = students.map((student) => {
+    return student.name + 'je';
+})
+console.log(littleStudents);
+
+let coursesList = students.find((student) => {
+    return student.name === 'Henk';
+})
+console.log(coursesList.name + ' doet ' + coursesList.course);
+
+let theo = students.filter((student) => {
+    return student.name === 'Theo';
+})
+
+console.log(theo[0].age);
+
+
